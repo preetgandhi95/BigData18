2f80f3da883e880f0b71c4d3aa40b59e37d4d726
Andrea Wang <ayw255@nyu.edu>
2018-05-05 19:00:55 -0400

    change code for command line usage

--- uniqueness_by_pandas.py
+++ uniqueness_by_pandas.py
@@ -1,15 +1,9 @@
 import sys
 import pandas as pd
 import numpy as np
-from pyspark.sql import SparkSession
-from pyspark.sql import SQLContext
-from pyspark.sql.functions import *
 import json
-spark = SparkSession.builder.appName("Python Spark SQL basic example").config("spark.some.config.option", "some-value").getOrCreate()
-from pyspark.sql import Row
-sc = spark.sparkContext
-threshold = 
-input_list = 
+import os
+input_list = sys.argv[1]
 for i in input_list : 
     data_path = i
     base = os.path.basename(data_path)
@@ -21,5 +15,4 @@ for i in input_list :
         u.append({'key':x, 'u_index': float("{0:.2f}".format(len(df[x].unique())/len(df[x]))), 'dataset': file_name})
 
     uniqueness = pd.DataFrame.from_dict(u)
-    uniqueness = uniqueness[uniqueness['u_index']>=threshold]
     uniqueness.to_csv('{0}_output.csv'.format(file_name),index = False)
