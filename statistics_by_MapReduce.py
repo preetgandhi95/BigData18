310b65b56b837759150facb8ceba47740f1e4488
Andrea Wang <ayw255@nyu.edu>
2018-05-07 20:51:14 -0400

    correct sys argv for column list

--- statistics_by_MapReduce.py
+++ statistics_by_MapReduce.py
@@ -7,7 +7,9 @@ sc = spark.sparkContext
 from csv import reader
 import pandas as pd
 import numpy as np
+import ast
 
+column_list = ast.literal_eval(sys.argv[2])
 
 base = os.path.basename(sys.argv[1])
 file_name = os.path.splitext(base)[0]
@@ -15,7 +17,7 @@ output = []
 lines = sc.textFile(sys.argv[1], 1).mapPartitions(lambda x: reader(x))
 total_rows =lines.map(lambda line:('line',1)).reduceByKey(lambda x,y: x+y)
 total_rows_count = total_rows.take(1)[0][1]
-column_list = np.array(sys.argv[2])
+
 for i in column_list:
     column_name = lines.take(1)[0][i]
     sum = lines.map(lambda line:(column_name, line[i])).reduceByKey(lambda x,y: x+y)
