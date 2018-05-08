907c550167f245c964d2f5c3516b21b166ae027a
Andrea Wang <ayw255@nyu.edu>
2018-05-07 20:27:34 -0400

    add output code

--- statistics_by_MapReduce.py
+++ statistics_by_MapReduce.py
@@ -8,6 +8,10 @@ from csv import reader
 import pandas as pd
 import numpy as np
 
+
+base = os.path.basename(sys.argv[1])
+file_name = os.path.splitext(base)[0]
+output = []
 lines = sc.textFile(sys.argv[1], 1).mapPartitions(lambda x: reader(x))
 total_rows =lines.map(lambda line:('line',1)).reduceByKey(lambda x,y: x+y)
 total_rows_count = total_rows.take(1)[0][1]
@@ -20,3 +24,6 @@ for i in range(column_list):
     std_parts = lines.map(lambda line:(column_name, (line[i]-mean)**2)).reduceByKey(lambda x,y: x+y)
     std = (std_parts.take(1)[0][1]/(total_rows_count-1))**0.5
     output.append({'column_name': column_name, 'mean': mean, 'std': std})
+
+output_df = pd.DataFrame.from_dict(output)
+output_df.to_csv('{0}_statistics_output.csv'.format(file_name), index = False)
